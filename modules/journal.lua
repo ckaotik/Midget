49242f46567931dbd3bd38bc432023faad27f6d9
ckaotik <ckaotik@gmail.com>
2013-06-15 20:14:10 +0200

    [DEV] New feature: Select spec for certain bosses and let Midget auto activate that loot spec

--- modules/journal.lua
+++ modules/journal.lua
@@ -56,40 +56,42 @@ end)
 -- ================================================
 local function ManageLootRoles(self, btn)
 	local bossButton = self:GetParent()
-	if not MidgetLocalDB.LFRLootRoles then MidgetLocalDB.LFRLootRoles = {} end
+	if not MidgetLocalDB.LFRLootSpecs then MidgetLocalDB.LFRLootSpecs = {} end
 
 	if btn == 'RightButton' then
-		MidgetLocalDB.LFRLootRoles[bossButton.encounterID] = nil
+		-- remove specialization preference
+		MidgetLocalDB.LFRLootSpecs[ bossButton.encounterID ] = nil
 	else
-		local role = MidgetLocalDB.LFRLootRoles[bossButton.encounterID]
-		if not role or role == "DAMAGER" then role = "TANK"
-		elseif role == "TANK" then role = "HEALER"
-		elseif role == "HEALER" then role = "DAMAGER"
-		end
-		MidgetLocalDB.LFRLootRoles[bossButton.encounterID] = role
+		-- switch to next specialization
+		-- TODO: save prefs per difficulty?
+		-- spec: local index of spec in player's spec choices
+		local spec = MidgetLocalDB.LFRLootSpecs[ bossButton.encounterID ] or 0 -- [ EJ_GetDifficulty() ]
+		spec = spec + 1
+		spec = GetSpecializationInfo(spec) and spec or 1
+		MidgetLocalDB.LFRLootSpecs[ bossButton.encounterID ] = spec
 	end
 end
-local function UpdateBossLootRoles(bossButton)
+local function UpdateLootSpecIcon(bossButton)
 	if not bossButton then return end
 	if not bossButton.roleButton then
-		local button = CreateFrame("Button", nil, bossButton)
-			  button:SetHighlightTexture("Interface\\Buttons\\UI-Common-MouseHilight")
-			  button:RegisterForClicks("LeftButtonUp", "RightButtonUp")
-			  button:SetScript("OnClick", function(self, btn)
-			  	ManageLootRoles(self, btn)
-			  	UpdateBossLootRoles(self:GetParent())
-			  end)
-			  button:SetPoint("TOPRIGHT", -4, -6)
-			  button:SetSize(16, 16)
+		local button = CreateFrame("Button", "$parentLootSpec", bossButton)
+		button:SetHighlightTexture("Interface\\Buttons\\UI-Common-MouseHilight")
+		button:RegisterForClicks("LeftButtonUp", "RightButtonUp")
+		button:SetScript("OnClick", function(self, btn)
+			ManageLootRoles(self, btn)
+			UpdateLootSpecIcon(self:GetParent())
+		end)
+		button:SetPoint("TOPRIGHT", -4, -6)
+		button:SetSize(16, 16)
+
 		bossButton.roleButton = button
 	end
 	local roleButton = bossButton.roleButton
 
-	local lootRole = MidgetLocalDB.LFRLootRoles and MidgetLocalDB.LFRLootRoles[bossButton.encounterID] or nil
-	if lootRole then
-		-- roleButton:SetNormalTexture("Interface\\GroupFrame\\UI-Group-"..lootRole.."Icon")
-		roleButton:SetNormalTexture("Interface\\LFGFrame\\UI-LFG-ICON-PORTRAITROLES")
-      	roleButton:GetNormalTexture():SetTexCoord(GetTexCoordsForRoleSmallCircle(lootRole))
+	local lootSpec = MidgetLocalDB.LFRLootSpecs and MidgetLocalDB.LFRLootSpecs[ bossButton.encounterID ] or nil
+	if lootSpec then
+		local specID, _, _, icon = GetSpecializationInfo(lootSpec)
+		roleButton:SetNormalTexture(icon)
 		roleButton:SetAlpha(1)
 	else
 		roleButton:SetNormalTexture("Interface\\Vehicles\\UI-Vehicles-Raid-Icon")
@@ -98,23 +100,93 @@ local function UpdateBossLootRoles(bossButton)
 	end
 end
 
-local selectedDifficulty = nil
 local function UpdateBossButtons()
 	local bossIndex, bossButton = 1, nil
 	local name, description, bossID, _, link = EJ_GetEncounterInfoByIndex(bossIndex)
 	while bossID do -- buttons should already exist from Blizzard's code!
 		bossButton = _G["EncounterJournalBossButton"..bossIndex]
-		UpdateBossLootRoles(bossButton)
+		UpdateLootSpecIcon(bossButton)
 
 		bossIndex = bossIndex + 1
 		name, description, bossID, _, link = EJ_GetEncounterInfoByIndex(bossIndex)
 	end
 end
 
+local function CheckUpdateLootSpec()
+	-- print('encounter started/changed/finished', UnitName('target'), UnitName('boss1'), UnitName('boss2'), UnitName('boss3'), UnitName('boss4'))
+
+	local instanceID = EJ_GetCurrentInstance()
+	local _, _, difficultyID = GetInstanceInfo() -- EJ_GetDifficulty()
+
+	-- instanceID only works if EJ is loaded, but if it isn't it's set to current instance, anyway
+	local encounterIndex = 1
+	local encounter, _, encounterID = EJ_GetEncounterInfoByIndex(encounterIndex, instanceID)
+	local targetName, bossName = UnitName('target'), nil
+	local matched = false
+	while encounterID do
+		local creatureIndex = 1
+		local _, creature = EJ_GetCreatureInfo(creatureIndex, encounterID)
+		while creature do
+			if targetName and targetName == creature then
+				-- print('target is boss', creature)
+				matched = true
+			else
+				for i=1,10 do
+					bossName = UnitName('boss'..i)
+					if bossName and bossName == creature then
+						-- print('boss', i, 'is boss', creature)
+						matched = true
+						break
+					end
+				end
+			end
+			-- don't need to check other creatures in this encounter
+			if matched then break end
+
+			creatureIndex = creatureIndex + 1
+			_, creature = EJ_GetCreatureInfo(creatureIndex, encounterID)
+		end
+
+		if matched then
+			if not lastEncounter or lastEncounter ~= encounterID then
+				lastEncounter = encounterID
+				break
+			else
+				return
+			end
+		end
+		encounterIndex = encounterIndex + 1
+		encounter, _, encounterID = EJ_GetEncounterInfoByIndex(encounterIndex, instanceID)
+	end
+
+	if matched then
+		local currentLoot = GetLootSpecialization()
+		local currentSpec = GetSpecializationInfo(GetSpecialization())
+		local specPreference = MidgetLocalDB.LFRLootSpecs and MidgetLocalDB.LFRLootSpecs[ encounterID ] or 0
+			  specPreference = GetSpecializationInfo(specPreference)
+		-- don't change if we have no preference set
+		if specPreference and (currentLoot == 0 and currentSpec ~= specPreference or currentLoot ~= specPreference) then
+			ns.Print('Changing loot spec to %s (%d)', GetSpecializationInfoByID(specPreference), specPreference)
+			SetLootSpecialization(specPreference)
+		else
+			-- ns.Print('already in correct spec')
+		end
+	end
+
+	--[[
+	/run local d=MidgetLocalDB.LFRLootSpecs; for eID,x in pairs(MidgetLocalDB.LFRLootRoles) do if x == "TANK" then d[eID] = 3 elseif x == "DAMAGER" then d[eID] = 1 else d[eID] = 4 end end
+	/run MidgetLocalDB.LFRLootRoles = nil
+	--]]
+
+	-- ns.UnregisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", "encounter_start")
+end
+
 -- events / hooks
+local selectedEncounter, selectedDifficulty
 hooksecurefunc("EncounterJournal_DisplayInstance", UpdateBossButtons)
 hooksecurefunc("EncounterJournal_DisplayEncounter", function(encounterID, noButton)
-	if selectedEncounter and selectedEncounter == EncounterJournal.encounterID then
+	if selectedEncounter and selectedEncounter == EncounterJournal.encounterID
+		and (not selectedDifficulty or selectedDifficulty == EJ_GetDifficulty()) then
 		-- unselect this boss without changing scroll position in boss list
 		NavBar_Reset(EncounterJournal.navBar)
 		selectedEncounter = nil
@@ -126,4 +198,15 @@ hooksecurefunc("EncounterJournal_DisplayEncounter", function(encounterID, noButt
 	else
 		selectedEncounter = encounterID
 	end
+	selectedDifficulty = EJ_GetDifficulty()
 end)
+
+local lastEncounter = nil
+ns.RegisterEvent("PLAYER_REGEN_ENABLED", function() lastEncounter = nil end, "encounter_end")
+ns.RegisterEvent("INSTANCE_ENCOUNTER_ENGAGE_UNIT", CheckUpdateLootSpec, "encounter_start")
+if BigWigsLoader then
+	BigWigsLoader:RegisterMessage("BigWigs_OnBossEngage", function( ... )
+		print('bigwigs boss enabled', ...)
+		CheckUpdateLootSpec()
+	end)
+end
