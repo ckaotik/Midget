b20fea0b0a750655f2818624ac9c3daef6b7e5f8
ckaotik <ckaotik@gmail.com>
2014-02-10 00:15:51 +0100

    Changed unit average item level calculation to closer to what Blizzard does (supposedly). Fixed item level not being requested properly once talent spec is available.

--- modules/tooltip.unit.lua
+++ modules/tooltip.unit.lua
@@ -1,6 +1,7 @@
 local addonName, ns, _ = ...
 
 -- GLOBALS: _G, UnitIsPlayer, UnitLevel, UnitGUID, CanInspect, GetInspectSpecialization, GetSpecializationInfoByID, GetInventoryItemLink, GetItemInfo, NotifyInspect, string
+-- TODO: fix heirloom item levels
 local LibItemUpgrade = LibStub("LibItemUpgradeInfo-1.0")
 
 -- keys and values are weak and may be garbage collected
@@ -10,29 +11,38 @@ local unitCache = setmetatable({}, {
 
 local unitTooltip, unitID = nil, nil
 local function INSPECT_READY(self, event, guid)
-	if not unitID or not UnitExists(unitID) then return end
+	if not unitID or not UnitExists(unitID) or UnitGUID(unitID) ~= guid then
+		unitCache[guid] = nil
+		return
+	end
+
 	local specID = GetInspectSpecialization(unitID)
 	local _, name, _, icon, _, role, _ = GetSpecializationInfoByID(specID)
+	local level = UnitLevel(unitID)
 
 	local itemLevels, mainHandLevel, isIncomplete = 0, nil, nil
 	local numSlots = _G.INVSLOT_LAST_EQUIPPED - 3 -- tabard, ranged, body don't provide iLvl
 	for slot = _G.INVSLOT_FIRST_EQUIPPED, _G.INVSLOT_LAST_EQUIPPED do
 		if slot ~= _G.INVSLOT_TABARD and slot ~= _G.INVSLOT_BODY and slot ~= _G.INVSLOT_RANGED then
 			local itemLink = GetInventoryItemLink(unitID, slot)
-			if not itemLink and GetInventoryItemID(unitID, slot) then
+			-- local itemQuality = GetInventoryItemQuality(unitID, slot)
+			local itemLevel
+			if itemLink then
+				itemLevel = LibItemUpgrade:GetUpgradedItemLevel(itemLink)
+			elseif GetInventoryItemID(unitID, slot) then
 				isIncomplete = true
 			end
-			local itemLevel = itemLink and LibItemUpgrade:GetUpgradedItemLevel(itemLink)
 			itemLevels = itemLevels + (itemLevel or 0)
 
 			-- apply main hand level if two hand and offhand empty
 			if slot == _G.INVSLOT_MAINHAND and itemLink then
 				local _, _, _, _, _, class, subclass, _, equipSlot = GetItemInfo(itemLink)
-				if equipSlot == 'INVTYPE_2HWEAPON' or equipSlot == 'INVTYPE_RANGEDRIGHT' then -- and not IsDualWielding() then
-					mainHandLevel = itemLevel
+				if equipSlot == 'INVTYPE_2HWEAPON' or equipSlot == 'INVTYPE_RANGEDRIGHT' then
+					mainHandLevel = itemLevel or 0
 				end
 			elseif slot == _G.INVSLOT_OFFHAND and not itemLink and mainHandLevel then
-				itemLevels = itemLevels + mainHandLevel
+				-- itemLevels = itemLevels + mainHandLevel
+				numSlots = numSlots - 1 -- blizzard calculates it this way presumably
 				mainHandLevel = nil
 			end
 		end
@@ -41,6 +51,7 @@ local function INSPECT_READY(self, event, guid)
 	local talentString = role and string.format('%s|T%s:0|t %s', _G['INLINE_'.. role ..'_ICON'], icon, name)
 	local levelString = (not isIncomplete and itemLevels > 0) and string.format('%d |T%s:0|t', itemLevels/numSlots, 'Interface\\GROUPFRAME\\UI-GROUP-MAINTANKICON')
 
+	-- store data and display
 	if not unitCache[guid] then unitCache[guid] = {} end
 	if talentString then unitCache[guid].talents = talentString end
 	if levelString  then unitCache[guid].levels  = levelString  end
@@ -48,7 +59,7 @@ local function INSPECT_READY(self, event, guid)
 	if isIncomplete then
 		-- request update. we could probably wait for GET_ITEM_INFO_RECEIVED instead to avoid using precious requests
 		NotifyInspect(unitID)
-	elseif unitTooltip and not unitTooltip.talentsAdded and UnitGUID(unitID) == guid then
+	elseif unitTooltip and not unitTooltip.talentsAdded then
 		-- add gathered data to tooltip
 		unitTooltip:AddDoubleLine(talentString or '', levelString or '')
 		unitTooltip.talentsAdded = true
@@ -56,13 +67,6 @@ local function INSPECT_READY(self, event, guid)
 	end
 end
 ns.RegisterEvent('INSPECT_READY', INSPECT_READY, 'unit_inspect')
-
--- when talents or equipment change, have the tooltip/cache update!
-local function UNIT_INVENTORY_CHANGED(...)
-	print(...)
-	-- unitCache[guid].numRequests = nil
-end
--- ns.RegisterEvent('UNIT_INVENTORY_CHANGED', UNIT_INVENTORY_CHANGED, 'unit_inventory')
 
 local function TooltipUnitInfo(tooltip)
 	local _, unit = tooltip:GetUnit()
@@ -82,12 +86,17 @@ local function TooltipUnitInfo(tooltip)
 	-- add talent and equipment info
 	if UnitLevel(unit) >= 10 and not tooltip.talentsAdded then
 		local guid = UnitGUID(unit)
-		if unitCache[guid] and unitCache[guid].talents then
+		if unitCache[guid] and (unitCache[guid].talents or unitCache[guid].levels) then
 			-- show in tooltip
 			tooltip:AddDoubleLine(unitCache[guid].talents or '', unitCache[guid].levels or '')
 			tooltip.talentsAdded = true
 			tooltip:Show()
-		elseif (UnitInParty(unit) or UnitInRaid(unit) or IsShiftKeyDown())
+			if unitCache[guid].talents and unitCache[guid].levels then
+				return
+			end
+		end
+
+		if (UnitInParty(unit) or UnitInRaid(unit) or IsShiftKeyDown())
 			and CanInspect(unit) and (not _G.InspectFrame or not _G.InspectFrame:IsShown()) then
 			unitTooltip = tooltip
 			unitID = unit
