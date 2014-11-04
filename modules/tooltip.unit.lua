9190d86827580a0dbee94a03ea90bd789766fb9d
ckaotik <ckaotik@gmail.com>
2014-11-04 21:07:10 +0100

    Cleanup with unit tooltips

--- modules/tooltip.unit.lua
+++ modules/tooltip.unit.lua
@@ -1,78 +1,23 @@
 local addonName, addon, _ = ...
 local plugin = addon:NewModule('UnitTooltip', 'AceEvent-3.0')
 
--- GLOBALS: _G, UnitIsPlayer, UnitLevel, UnitGUID, CanInspect, GetInspectSpecialization, GetSpecializationInfoByID, GetInventoryItemLink, GetItemInfo, NotifyInspect, string
--- TODO: fix heirloom item levels
-local LibItemUpgrade = LibStub("LibItemUpgradeInfo-1.0")
+-- GLOBALS: _G, LibStub, RED_FONT_COLOR, GameTooltip, hooksecurefunc
+-- GLOBALS: UnitIsPlayer, UnitLevel, UnitGUID, CanInspect, GetInspectSpecialization, GetSpecializationInfoByID, GetInventoryItemLink, GetItemInfo, NotifyInspect, UnitInParty, UnitInRaid
+
+local LibItemUpgrade = LibStub('LibItemUpgradeInfo-1.0')
 
 -- keys and values are weak and may be garbage collected
 local unitCache = setmetatable({}, {
-	__mode = "kv",
+	__mode = 'kv',
 })
-
-local unitTooltip, unitID = nil, nil
-local function INSPECT_READY(event, guid)
-	if not unitID or not UnitExists(unitID) or UnitGUID(unitID) ~= guid then
-		unitCache[guid] = nil
-		return
-	end
-
-	local specID = GetInspectSpecialization(unitID)
-	local _, name, _, icon, _, role, _ = GetSpecializationInfoByID(specID)
-	local level = UnitLevel(unitID)
-
-	local itemLevels, mainHandLevel, isIncomplete = 0, nil, nil
-	local numSlots = _G.INVSLOT_LAST_EQUIPPED - 3 -- tabard, ranged, body don't provide iLvl
-	for slot = _G.INVSLOT_FIRST_EQUIPPED, _G.INVSLOT_LAST_EQUIPPED do
-		if slot ~= _G.INVSLOT_TABARD and slot ~= _G.INVSLOT_BODY and slot ~= _G.INVSLOT_RANGED then
-			local itemLink = GetInventoryItemLink(unitID, slot)
-			-- local itemQuality = GetInventoryItemQuality(unitID, slot)
-			local itemLevel
-			if itemLink then
-				itemLevel = LibItemUpgrade:GetUpgradedItemLevel(itemLink)
-			elseif GetInventoryItemID(unitID, slot) then
-				isIncomplete = true
-			end
-			itemLevels = itemLevels + (itemLevel or 0)
-
-			-- apply main hand level if two hand and offhand empty
-			if slot == _G.INVSLOT_MAINHAND and itemLink then
-				local _, _, _, _, _, class, subclass, _, equipSlot = GetItemInfo(itemLink)
-				if equipSlot == 'INVTYPE_2HWEAPON' or equipSlot == 'INVTYPE_RANGEDRIGHT' then
-					mainHandLevel = itemLevel or 0
-				end
-			elseif slot == _G.INVSLOT_OFFHAND and not itemLink and mainHandLevel then
-				-- itemLevels = itemLevels + mainHandLevel
-				numSlots = numSlots - 1 -- blizzard calculates it this way presumably
-				mainHandLevel = nil
-			end
-		end
-	end
-
-	local talentString = role and string.format('%s|T%s:0|t %s', _G['INLINE_'.. role ..'_ICON'], icon, name)
-	local levelString = (not isIncomplete and itemLevels > 0) and string.format('%d |T%s:0|t', itemLevels/numSlots, 'Interface\\GROUPFRAME\\UI-GROUP-MAINTANKICON')
-
-	-- store data and display
-	if not unitCache[guid] then unitCache[guid] = {} end
-	if talentString then unitCache[guid].talents = talentString end
-	if levelString  then unitCache[guid].levels  = levelString  end
-
-	if isIncomplete then
-		-- request update. we could probably wait for GET_ITEM_INFO_RECEIVED instead to avoid using precious requests
-		NotifyInspect(unitID)
-	elseif unitTooltip and not unitTooltip.talentsAdded then
-		-- add gathered data to tooltip
-		unitTooltip:AddDoubleLine(talentString or '', levelString or '')
-		unitTooltip.talentsAdded = true
-		unitTooltip:Show()
-	end
-end
 
 local function TooltipUnitInfo(tooltip)
 	local _, unit = tooltip:GetUnit()
-	if not unit or not UnitIsPlayer(unit) then return end
+	if not unit then return end
 
 	-- move faction text up one line
+	--[[
+	local factionGroup, faction = UnitFactionGroup(unit)
 	local faction = _G[tooltip:GetName()..'TextLeft4']
 	local factionText = faction and faction:GetText()
 	if faction and factionText and factionText ~= '' then
@@ -81,28 +26,99 @@ local function TooltipUnitInfo(tooltip)
 		      newFaction:SetText(factionColor .. faction:GetText() .. '|r')
 		      newFaction:Show()
 		faction:SetText(nil)
-	end
+	end --]]
+
+	local guid = UnitGUID(unit)
+	local data = unitCache[guid]
 
 	-- add talent and equipment info
-	if UnitLevel(unit) >= 10 and not tooltip.talentsAdded then
-		local guid = UnitGUID(unit)
-		if unitCache[guid] and (unitCache[guid].talents or unitCache[guid].levels) then
-			-- show in tooltip
-			tooltip:AddDoubleLine(unitCache[guid].talents or '', unitCache[guid].levels or '')
-			tooltip.talentsAdded = true
-			tooltip:Show()
-			if unitCache[guid].talents and unitCache[guid].levels then
-				return
-			end
+	if UnitIsPlayer(unit) and UnitLevel(unit) >= 10 and data then
+		-- TODO: notify when raid role does not match spec role
+		-- local role, isLeader = UnitGroupRolesAssigned(unit), UnitIsGroupLeader(unit)
+
+		local _, specName, _, specIcon, _, role, _ = GetSpecializationInfoByID(data.spec or 0)
+		local left   = role and ('%s|T%s:0|t %s'):format(_G['INLINE_'.. role ..'_ICON'], specIcon, specName) or ''
+		local ilevel = data.ilevel
+		local right  = ilevel > 0 and ('%d |T%s:0|t'):format(ilevel, 'Interface\\GROUPFRAME\\UI-GROUP-MAINTANKICON') or ''
+
+		if not data.complete then
+			right = _G.RED_FONT_COLOR_CODE .. right .. '|r'
 		end
 
-		if (UnitInParty(unit) or UnitInRaid(unit) or IsShiftKeyDown())
-			and CanInspect(unit) and (not _G.InspectFrame or not _G.InspectFrame:IsShown()) then
-			unitTooltip = tooltip
-			unitID = unit
-			NotifyInspect(unit)
+		if tooltip.talents then
+			_G[tooltip:GetName()..'TextLeft'..tooltip.talents]:SetText(left)
+			_G[tooltip:GetName()..'TextRight'..tooltip.talents]:SetText(right)
+		else
+			tooltip:AddDoubleLine(left, right)
+			tooltip.talents = tooltip:NumLines()
+		end
+		tooltip:Show()
+	end
+	if (not data or not data.complete) and (not _G.InspectFrame or not _G.InspectFrame:IsShown())
+		and CanInspect(unit) and (UnitInParty(unit) or UnitInRaid(unit) or IsShiftKeyDown()) then
+		NotifyInspect(unit)
+	end
+
+	-- color health bar by class
+	if UnitIsPlayer(unit) and UnitHealth(unit) > 0 and not UnitIsDeadOrGhost(unit) then
+		local _, class = UnitClass(unit)
+		local color = (_G.CUSTOM_CLASS_COLORS or _G.RAID_CLASS_COLORS)[class]
+		local statusBar = _G[tooltip:GetName()..'StatusBar']
+		if statusBar then
+			statusBar:SetStatusBarColor(color.r, color.g, color.b)
 		end
 	end
+end
+
+function plugin:INSPECT_READY(event, guid)
+	local className, class, _, _, gender, unitName, realm = GetPlayerInfoByGUID(guid)
+	-- this is not a player
+	if not className or not unitCache[guid] then return end
+
+	local unit = unitCache[guid].unit
+	if unit then
+		local specID = GetInspectSpecialization(unit)
+		unitCache[guid].spec = specID ~= 0 and specID or unitCache[guid].spec
+
+		-- TODO: fix heirloom item levels
+		local itemLevels, mainHandLevel, complete = 0, nil, true
+		local numSlots = _G.INVSLOT_LAST_EQUIPPED - 3 -- tabard, ranged, body don't provide iLvl
+		for slot = _G.INVSLOT_FIRST_EQUIPPED, _G.INVSLOT_LAST_EQUIPPED do
+			if slot ~= _G.INVSLOT_TABARD and slot ~= _G.INVSLOT_BODY and slot ~= _G.INVSLOT_RANGED then
+				local itemID   = GetInventoryItemID(unit, slot)
+				local itemLink = GetInventoryItemLink(unit, slot)
+				if itemID and not itemLink then
+					-- item data is not available
+					complete = false
+					itemLink = 'item:'..itemID
+				end
+
+				local itemLevel = itemLink and LibItemUpgrade:GetUpgradedItemLevel(itemLink) or 0
+				itemLevels = itemLevels + itemLevel
+
+				-- apply main hand level if two hand and offhand empty
+				if slot == _G.INVSLOT_MAINHAND and itemLink then
+					local _, _, _, _, _, class, subclass, _, equipSlot = GetItemInfo(itemLink)
+					if equipSlot == 'INVTYPE_2HWEAPON' or equipSlot == 'INVTYPE_RANGEDRIGHT' then
+						mainHandLevel = itemLevel
+					end
+				elseif slot == _G.INVSLOT_OFFHAND and not itemLink and mainHandLevel then
+					-- blizzard supposedly calculates it this way
+					numSlots = numSlots - 1
+					mainHandLevel = nil
+				end
+			end
+		end
+		unitCache[guid].ilevel = itemLevels/numSlots or unitCache[guid].ilevel
+		unitCache[guid].complete = complete
+	end
+
+	-- update tooltip
+	TooltipUnitInfo(GameTooltip)
+end
+
+function plugin:PLAYER_SPECIALIZATION_CHANGED()
+	wipe(unitCache)
 end
 
 -- display item specs
@@ -128,35 +144,32 @@ local function TooltipItemInfo(self)
 end
 
 function plugin:OnEnable()
-	self:RegisterEvent('INSPECT_READY', INSPECT_READY)
-
-	-- unit info
-	GameTooltip:HookScript('OnTooltipSetUnit', TooltipUnitInfo)
-	GameTooltip:HookScript('OnTooltipCleared', function(self)
-		self.talentsAdded = nil
-		unitID = nil
-		unitTooltip = nil
-	end)
-
-	-- item info
-	GameTooltip:HookScript('OnTooltipSetItem', TooltipItemInfo)
-	ItemRefTooltip:HookScript('OnTooltipSetItem', TooltipItemInfo)
-	if ShoppingTooltip1 then ShoppingTooltip1:HookScript('OnTooltipSetItem', TooltipItemInfo) end
-	if ShoppingTooltip2 then ShoppingTooltip2:HookScript('OnTooltipSetItem', TooltipItemInfo) end
-
 	-- tooltip position
 	hooksecurefunc('GameTooltip_SetDefaultAnchor', function(self, parent)
 		self:SetPoint('BOTTOMRIGHT', 'UIParent', 'BOTTOMRIGHT', -72, 152)
 	end)
 
-	GameTooltip:HookScript('OnTooltipSetUnit', function(self)
-		local _, unit = GameTooltip:GetUnit()
-		local r, g, b
-		if UnitIsPlayer(unit) and UnitHealth(unit) > 0 and not UnitIsDeadOrGhost(unit) then
-			local _, class = UnitClass(unit)
-			local color = (CUSTOM_CLASS_COLORS or RAID_CLASS_COLORS)[class]
-			self.statusBar:SetStatusBarColor(color.r, color.g, color.b)
+	-- item info
+	for _, tipName in pairs({'GameTooltip', 'ItemRefTooltip', 'ShoppingTooltip1', 'ShoppingTooltip2'}) do
+		local tooltip = _G[tipName]
+		if tooltip then
+			tooltip:HookScript('OnTooltipSetItem', TooltipItemInfo)
 		end
+	end
+
+	-- unit info
+	self:RegisterEvent('INSPECT_READY')
+	self:RegisterEvent('PLAYER_SPECIALIZATION_CHANGED')
+	GameTooltip:HookScript('OnTooltipSetUnit', TooltipUnitInfo)
+	GameTooltip:HookScript('OnTooltipCleared', function(self)
+		self.talents = nil
+	end)
+
+	hooksecurefunc('NotifyInspect', function(unit)
+		if not UnitIsPlayer(unit) then return end
+		local guid = UnitGUID(unit)
+		if not unitCache[guid] then unitCache[guid] = {} end
+		unitCache[guid].unit = unit
 	end)
 end
 
