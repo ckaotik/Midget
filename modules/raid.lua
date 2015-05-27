81056d117ada18197b46c20ffdf39ddfa5d9270d
ckaotik <ckaotik@gmail.com>
2015-05-27 19:53:22 +0200

    Raid tracker: Get colors from caster class

--- modules/raid.lua
+++ modules/raid.lua
@@ -5,67 +5,14 @@ local plugin = addon:NewModule('RaidTracker', 'AceEvent-3.0')
 -- GLOBALS:
 -- GLOBALS: hooksecurefunc
 
-local raidBuffColors = {
-	-- _G.RAID_BUFF_1 stat multiplier
-	[  1126] = _G.RAID_CLASS_COLORS['DRUID'], -- Mark of the Wild
-	[ 20217] = _G.RAID_CLASS_COLORS['PALADIN'], -- Blessing of Kings
-	[117666] = _G.RAID_CLASS_COLORS['MONK'], -- Legacy of the Emperor
-	[ 90363] = _G.RAID_CLASS_COLORS['HUNTER'], -- Embrace of the Shale Spider
-	-- _G.RAID_BUFF_2 stamina
-	[   469] = _G.RAID_CLASS_COLORS['WARRIOR'], -- Commanding Shout
-	[ 21562] = _G.RAID_CLASS_COLORS['PRIEST'], -- Power Word: Fortitude
-	[ 90364] = _G.RAID_CLASS_COLORS['HUNTER'], -- Qiraji Fortitude
-	[109773] = _G.RAID_CLASS_COLORS['WARLOCK'], -- Dark Intent
-	[ 86507] = {r = 1, g = 1, b = 1, a = 1}, -- Runescroll of Fortitude II [Inscription]
-	[ 69377] = {r = 1, g = 1, b = 1, a = 1}, -- Runescroll of Fortitude I [Inscription]
-	-- _G.RAID_BUFF_3 -- attack power
-	[  6673] = _G.RAID_CLASS_COLORS['WARRIOR'], -- Batte Shout
-	[ 19506] = _G.RAID_CLASS_COLORS['HUNTER'], -- Trueshot Aura
-	[ 57330] = _G.RAID_CLASS_COLORS['DEATHKNIGHT'], -- Horn of Winter
-	-- _G.RAID_BUFF_4 attack speed
-	[ 30809] = _G.RAID_CLASS_COLORS['SHAMAN'], -- Unleashed Rage
-	[ 55610] = _G.RAID_CLASS_COLORS['DEATHKNIGHT'], -- Unholy Aura
-	[113742] = _G.RAID_CLASS_COLORS['ROGUE'], -- Swiftblade's Cunning
-	[128432] = _G.RAID_CLASS_COLORS['HUNTER'], -- Cackling Howl
-	[128433] = _G.RAID_CLASS_COLORS['HUNTER'], -- Serpent's Swiftness
-	-- _G.RAID_BUFF_5 spell power
-	[  1459] = _G.RAID_CLASS_COLORS['MAGE'], -- Arcane Brilliance
-	[ 61316] = _G.RAID_CLASS_COLORS['MAGE'], -- Dalaran Brilliance
-	[ 77747] = _G.RAID_CLASS_COLORS['SHAMAN'], -- Burning Wrath
-	-- [109773] = _G.RAID_CLASS_COLORS['WARLOCK'], -- Dark Intent
-	[126309] = _G.RAID_CLASS_COLORS['HUNTER'], -- Still Water
-	-- _G.RAID_BUFF_6 spell haste
-	[ 15473] = _G.RAID_CLASS_COLORS['PRIEST'], -- Shadowform
-	[ 24907] = _G.RAID_CLASS_COLORS['DRUID'], -- Moonkin Aura
-	[ 51470] = _G.RAID_CLASS_COLORS['SHAMAN'], -- Elemental Oath
-	[ 49868] = _G.RAID_CLASS_COLORS['HUNTER'], -- Mind Quickening
-	[135678] = _G.RAID_CLASS_COLORS['HUNTER'], -- Energizing Spores
-	-- _G.RAID_BUFF_7 critical strike chance
-	-- [  1459] = _G.RAID_CLASS_COLORS['MAGE'], -- Arcane Brilliance
-	[ 17007] = _G.RAID_CLASS_COLORS['DRUID'], -- Leader of the Pack
-	[ 24604] = _G.RAID_CLASS_COLORS['HUNTER'], -- Furious Howl
-	-- [ 61316] = _G.RAID_CLASS_COLORS['MAGE'], -- Dalaran Brilliance
-	[ 90309] = _G.RAID_CLASS_COLORS['HUNTER'], -- Terrifying Roar
-	[ 97229] = _G.RAID_CLASS_COLORS['HUNTER'], -- Bellowing Roar
-	[116781] = _G.RAID_CLASS_COLORS['MONK'], -- Legacy of the White Tiger
-	-- [126309] = _G.RAID_CLASS_COLORS['HUNTER'], -- Still Water
-	[126373] = _G.RAID_CLASS_COLORS['HUNTER'], -- Fearless Roar
-	-- _G.RAID_BUFF_8 mastery
-	[ 19740] = _G.RAID_CLASS_COLORS['PALADIN'], -- Blessing of Might
-	[116956] = _G.RAID_CLASS_COLORS['SHAMAN'], -- Grace of Air
-	[ 93435] = _G.RAID_CLASS_COLORS['HUNTER'], -- Roar of Courage
-	[128997] = _G.RAID_CLASS_COLORS['HUNTER'], -- Spirit Beast Blessing
-}
-
 local function OnEnterRaidBuff(self)
 	GameTooltip:SetOwner(self)
 	local index = self:GetID()
 	local spellName, _, _, _, _, spellID = GetRaidBuffTrayAuraInfo(index)
-	local effect = _G['RAID_BUFF_'..index]:gsub('-\n', '')
 	if spellID then
 		GameTooltip:SetUnitConsolidatedBuff('player', index)
-		GameTooltip:AddLine(string.format(_G.ITEM_SPELL_EFFECT, effect), 1, 1, 1, true)
 	else
+		local effect = _G['RAID_BUFF_'..index]:gsub('-\n', '')
 		GameTooltip:AddLine(string.format(_G.ITEM_MISSING, effect), 1, 0, 0, true)
 	end
 	GameTooltip:Show()
@@ -112,14 +59,16 @@ end
 
 function plugin:UNIT_AURA(event, unit)
 	if unit ~= 'player' then return end
-	local enabled = MidgetDB.showRaidBuffIndicators
+	local enabled = addon.db.profile.showRaidBuffIndicators
 	for index = 1, _G.NUM_LE_RAID_BUFF_TYPES do
 		local name, rank, texture, duration, expiration, spellID, slot = GetRaidBuffTrayAuraInfo(index)
 		local icon = self.indicators[index].icon
 		if not name or not enabled then
 			icon:Hide()
 		else
-			local color = raidBuffColors[spellID] or _G.NORMAL_FONT_COLOR
+			local _, _, _, _, _, _, _, caster = UnitBuff('player', name)
+			local class = caster and select(2, UnitClass(caster))
+			local color = class and RAID_CLASS_COLORS[class] or _G.NORMAL_FONT_COLOR
 			icon:SetVertexColor(color.r, color.g, color.b, color.a)
 			icon:Show()
 		end
