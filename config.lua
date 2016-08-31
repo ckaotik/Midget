1063e4c16ac5e218ee7a6092d856bbe2bc4385db
ckaotik <ckaotik@gmail.com>
2016-08-31 11:43:10 +0200

    Updated config

--- config.lua
+++ config.lua
@@ -1,48 +1,59 @@
 local addonName, addon, _ = ...
+local AceConfigDialog = LibStub('AceConfigDialog-3.0')
 
-local function OpenConfiguration(self, args)
-	-- remove placeholder configuration panel
-	for i, panel in ipairs(_G.INTERFACEOPTIONS_ADDONCATEGORIES) do
-		if panel == self then
-			tremove(INTERFACEOPTIONS_ADDONCATEGORIES, i)
-			break
-		end
-	end
-	self:SetScript('OnShow', nil)
-	self:Hide()
-
+local function GetConfigurationVariables()
 	local types = {
 		petBattleTeams = '*none*',
 		LFRLootSpecs = '*none*',
 	}
 
-	-- initialize panel
-	LibStub('LibDualSpec-1.0'):EnhanceDatabase(addon.db, addonName)
-	LibStub('AceConfig-3.0'):RegisterOptionsTable(addonName, {
-		type = 'group',
-		args = {
-			general  = LibStub('LibOptionsGenerate-1.0'):GetOptionsTable(addon.db, types),
-			profiles = LibStub('AceDBOptions-3.0'):GetOptionsTable(addon.db)
-		},
-	})
-	local AceConfigDialog = LibStub('AceConfigDialog-3.0')
-	AceConfigDialog:AddToBlizOptions(addonName, nil, nil, 'general')
-	AceConfigDialog:AddToBlizOptions(addonName, 'Profiles', addonName, 'profiles')
+	local locale = {}
 
-	OpenConfiguration = function(panel, args)
-		InterfaceOptionsFrame_OpenToCategory(addonName)
-	end
-	OpenConfiguration(self, args)
+	-- variable, typeMappings, L, includeNamespaces, callback
+	return addon.db, types, locale, true, nil
 end
 
--- create a fake configuration panel
-local panel = CreateFrame('Frame')
-	  panel.name = addonName
-	  panel:Hide()
-	  panel:SetScript('OnShow', OpenConfiguration)
-InterfaceOptions_AddCategory(panel)
+local function InitializeConfiguration(self, args)
+	local AceConfig = LibStub('AceConfig-3.0')
+
+	LibStub('LibDualSpec-1.0'):EnhanceDatabase(addon.db, addonName)
+
+	-- Initialize main panel.
+	local optionsTable = LibStub('LibOptionsGenerate-1.0'):GetOptionsTable(GetConfigurationVariables())
+	      optionsTable.name = addonName
+	if AddConfigurationExtras then AddConfigurationExtras(optionsTable) end
+	AceConfig:RegisterOptionsTable(addonName, optionsTable)
+
+
+	-- Add panels for submodules.
+	local AceConfigRegistry = LibStub('AceConfigRegistry-3.0')
+	for name, subModule in addon:IterateModules() do
+		if AceConfigRegistry.tables[subModule.name] then
+			AceConfigDialog:AddToBlizOptions(subModule.name, name, addonName)
+		end
+	end
+
+	if addon.db.defaults and addon.db.defaults.profile and next(addon.db.defaults.profile) then
+		-- Add panel for profile settings.
+		local profileOptions = LibStub('AceDBOptions-3.0'):GetOptionsTable(addon.db)
+		profileOptions.name = addonName .. ' - ' .. profileOptions.name
+		AceConfig:RegisterOptionsTable(addonName..'_profiles', profileOptions)
+		AceConfigDialog:AddToBlizOptions(addonName..'_profiles', 'Profiles', addonName)
+	end
+
+	-- Restore original OnShow handler.
+	self:SetScript('OnShow', self.origOnShow)
+	self.origOnShow = nil
+
+	InterfaceAddOnsList_Update()
+	InterfaceOptionsList_DisplayPanel(self)
+end
+
+-- Create a placeholder configuration panel.
+local panel = AceConfigDialog:AddToBlizOptions(addonName)
+panel.origOnShow = panel:GetScript('OnShow')
+panel:SetScript('OnShow', InitializeConfiguration)
 
 -- use slash command to toggle config
-local slashName = addonName:upper()
-_G['SLASH_'..slashName..'1'] = '/'..addonName
-_G.SlashCmdList[slashName] = function(args) OpenConfiguration(panel, args) end
+_G['SLASH_'..addonName..'1'] = '/'..addonName
+_G.SlashCmdList[addonName] = function(args) InterfaceOptionsFrame_OpenToCategory(addonName) end
